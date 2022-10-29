e6f006ab84fae174696aef3cd102ce067e04c60c
yamato <yamatotrumpet@yahoo.co.jp>
2022-10-29 17:37:31 +0900

    CMakeプロジェクト追加

---
+++ CMakeProject/template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "HelloWorld." << endl;
+
+	string s;
+	cin >> s;
+	cout << "input : " << s << endl;
+}
---
+++ CMakeProject/test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cout << "test." << endl;
+
+	string s;
+	cin >> s;
+	cout << "input : " << s << endl;
+}
